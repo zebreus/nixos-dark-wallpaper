e1c640083d551fdaf6e370c0c08a1108b4eae9ae
Zebreus <lennart@zebre.us>
2024-04-06 12:49:47 +0200

    Try to add gnome wallpapers config

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A nixos gnome wallpaper";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      rec {
+        pkgs = import nixpkgs { inherit system; };
+
+        name = "nixos-wallpaper";
+        packages.default = (with pkgs;   stdenv.mkDerivation
+          rec {
+            pname = "nixos-wallpaper";
+            version = "0.0.0";
+
+            src = ./.;
+
+            buildInputs = [
+            ];
+
+            installPhase = ''
+              mkdir -p $out/share/backgrounds/gnome
+              cp $src/thinknix-d.svg $out/share/backgrounds/gnome/thinknix-dark.svg
+              cp $src/nix-d.svg $out/share/backgrounds/gnome/thinknix-light.svg
+              mkdir -p $out/share/gnome-background-properties
+              cp $src/thinknix.xml $out/share/gnome-background-properties/thinknix.xml
+            '';
+
+            meta.mainProgram = "${pname}";
+          });
+      }
+    );
+}
